f615586681bf36902f426f7cb43f384b5b3bd374
Hassanraza12345 <hr12302000@gmail.com>
2024-10-24 06:10:10 -0700

    1

--- Api/Api/settings.py
+++ Api/Api/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-$r=t=9wh8$tfh81p6q!imj+1n%uw!n_v!vy@d9el%t#hmu_+)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mydjango.onrender.com','127.0.0.1']
 
 
 # Application definition
